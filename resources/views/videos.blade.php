a41f5ca3b83c94508787a3e2ca0dd368318e6757
snerva <selenenerva@live.it>
2022-12-16 19:33:51 +0100

    bonus

---
+++ resources/views/videos.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="main_contents">
+    <div class="main_banner"></div>
+    <div class="container">
+        <div class="label">
+            <h2>videos</h2>
+        </div>
+    </div>
+    <div class="menu_panel">
+        <div class="container d-flex justify-content-evenly align-items-center">
+            <div class="mp digital_comics">
+                <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" alt="">
+                <a href="#">digital comics</a>
+            </div>
+            <div class="mp merchandise">
+                <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" alt="">
+                <a href="#">dc merchandise</a>
+            </div>
+            <div class="mp subscription">
+                <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" alt="">
+                <a href="#">subscription</a>
+            </div>
+            <div class="mp shop_locator">
+                <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" alt="">
+                <a href="#">comic shop locator</a>
+            </div>
+            <div class="mp power_visa">
+                <img src="{{Vite::asset('resources/images/buy-dc-power-visa.svg')}}" alt="">
+                <a href="#">dc power visa</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,39 @@ Route::get('/', function () {
     $comics = config('db.comics');
     return view('comics', compact('comics'));
 })->name('comics');
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
+
+Route::get('/tv', function () {
+    return view('tv');
+})->name('tv');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+Route::get('/collectibles', function () {
+    return view('collectibles');
+})->name('collectibles');
+
+Route::get('/videos', function () {
+    return view('videos');
+})->name('videos');
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
