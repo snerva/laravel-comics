b077cad990bfbb30afae394db7131df1c7ec8c2b
snerva <selenenerva@live.it>
2022-12-16 17:06:02 +0100

    add footer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,22 @@
-<header>
-    <!--header here-->
+<header class="bg-white">
+    <div class="container">
+        <div class="navbar_header d-flex justify-content-between align-items-center">
+            <div class="logo p-3">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png')}}" alt="">
+            </div>
+
+            <div class="menu_header d-flex">
+                <a href="">characters</a>
+                <a class="active" href="">comics</a>
+                <a href="">movies</a>
+                <a href="">tv</a>
+                <a href="">games</a>
+                <a href="">collectibles</a>
+                <a href="">videos</a>
+                <a href="">fans</a>
+                <a href="">news</a>
+                <a href="">shop</a>
+            </div>
+        </div>
+    </div>
 </header>
